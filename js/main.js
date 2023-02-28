0344ed82548a9cae72b079e567d8c817741bbffc
Beni Randy Harold <haroldbeni111997@gmail.com>
2023-02-28 17:25:22 +0100

    esercizio dei dadi,finito

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const authorizedEmails = ['beni@flick.it', 'marcoblanco@flick.it', 'giuseppegerx@flick.it'];
+
+let userToVerify = prompt('write your email');
+
+console.log(authorizedEmails);
+console.log(userToVerify);
+
+
+for (i = 0; i < authorizedEmails.length; i++) {
+
+    let user = authorizedEmails[i];
+
+    if (userToVerify == user) {
+        console.log('Welcome');
+
+    } else if (userToVerify !== user) {
+        console.log('you cannot access!');
+    }
+    else{ console.log('loading')}
+}
